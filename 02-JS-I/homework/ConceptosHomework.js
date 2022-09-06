b2d1ae30afd7ef14dac29b999c75aa8b148eda46
jeromanp <jeduardo.roman69@gmail.com>
2022-09-05 23:40:12 -0500

    TAREA FINALIZADA

---
+++ 02-JS-I/homework/ConceptosHomework.js
@@ -0,0 +1,24 @@
+//CONCEPTOS HOMEWORK//
+
+//VARIABLE
+
+//Una variable es cualquier valor que puede tomar una cosa, pudiendo ser una palabra, un numero, el resultado de una operacion, etc.
+
+//STRING
+
+//Un string son letras que pueden o no formar una palabra, puede ser de cualquier tamaño.
+
+//FUNCIONES (ARGUMENTOS, RETURN)
+
+//Un argumento son diferentes valores que queremos se muestren cuando se invoquen.
+
+//La funcion RETURN arroja siempre una accion a tomar en caso de que la sentencia anterior se cumpla.
+
+//DECLARACIONES IF
+
+//Una declaracion IF, suele delimitar las acciones que hara una funcion, en caso de que no, se brinca a la otra y asi sucesivamente 
+//hasta finalizar, obteniendo asi un resultado.
+
+//VALORES BOOLEANOS (TRUE, FALSE)
+
+//Basicamente es un "si" o un "no" dentro de una funcion, si se cumple o no.
