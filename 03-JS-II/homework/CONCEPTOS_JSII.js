1dc67b75c1ff73715ac98b3157d4f9a6c8349ecf
jeromanp <jeduardo.roman69@gmail.com>
2022-09-06 18:20:47 -0500

    CONCEPTOS FINALIZADOS JS II

---
+++ 03-JS-II/homework/CONCEPTOS_JSII.js
@@ -0,0 +1,20 @@
+//CONCEPTOS JS II
+
+//FOR
+
+//Es una función que nos puede ayudar a crear ciclos sobre una cierta cosa, teniendo en cuenta sentenciar patra no generar
+//un bucle infinito, no olvidar suma uno en uno a la variable.
+
+//AND (&&)
+
+//Es una operacion booleana que, otorga un valor true si y solo si, los dos valores comparandos son true. En caso contrario
+//sera una false.
+
+//OR (||)
+
+//Es una operacion booleana que, otorga un valor true si uno o ambos valores comparados son true. EN caso contrario sera false.
+
+//NOT (!)
+
+//Es una operacion booleana que, devuelve el contrario de lo que suceda en la sentencia. Si es true el resultado sera false
+//y viceversa.
