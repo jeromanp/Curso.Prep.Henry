4c2d0abf4089bc53b802e94f23ffd3ea56a87b62
jeromanp <jeduardo.roman69@gmail.com>
2022-09-06 23:41:38 -0500

    CONCEPTOS_JS_III

---
+++ 04-JS-III/homework/CONCEPTOS_JS_III.js
@@ -0,0 +1,6 @@
+// CONCEPTOS
+
+//ARRAY
+
+// Un Array es una coleccion de datos, los cuales se nombrab a traves de una unica variable y que pueden ser llamados
+//cuando se les requiera, de acuerdo a la posicion que ocupa.
