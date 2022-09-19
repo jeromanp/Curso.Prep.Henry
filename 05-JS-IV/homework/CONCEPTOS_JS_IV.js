5e32683a97bc3ee493e246574a5fc70c341ed4b5
jeromanp <jeduardo.roman69@gmail.com>
2022-09-19 12:29:53 -0500

    Conceptos finalizados js IV

---
+++ 05-JS-IV/homework/CONCEPTOS_JS_IV.js
@@ -0,0 +1,32 @@
+                //Objetos
+
+//En JS, un objeto representa el contenido de diversas características sobre una cosa
+//para ello, se denota la clave de la característica y el valor de la misma para asociarla al objeto.
+
+                //Prpiedades
+
+//Son características que definen a una cosa para representarla y distinguirla de las demás.
+
+               ///Métodos
+
+//Son funciones declaradas dentro de un objeto.
+
+               ///Bucle For..in
+
+//Similar al bucle for, solo que esta se declara en objetos para buscar, retornar o convocar a una clave junto con su valor.
+
+
+                  //Notación de puntos vs notación de corchetes
+
+//Son dos distintas formas de nombrar, y / o agregar una característica de un objetos.
+//La notación con punto, es mas fácil y simple de ocuparla ya que se nombra al objeto, seguido de un punto y el valor a buscar.
+
+//                           <objeto>.<clave>
+
+//La notación con corchetes emplea corchetes para nombrar la clave a identificar.
+
+//                         <objeto>[clave]
+
+
+//Cada una cuenta con ventajas y desventajas y es reponsabilidad del programador, saberlas aplicar correctamente
+
